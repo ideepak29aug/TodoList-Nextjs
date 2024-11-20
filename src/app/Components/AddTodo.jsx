4d954912621556a8ff71cbe992299f5a9885d5d6
ideepak_29aug <djangir2908@gmail.com>
2024-11-20 12:29:46 +0530

    error solve 3

--- src/app/Components/AddTodo.jsx
+++ src/app/Components/AddTodo.jsx
@@ -2,7 +2,7 @@
 
 import { React, useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addTodo, updateTodo, clearEditTodo } from '@/app/features/todo/TodoSlice';
+import { addTodo, updateTodo, clearEditTodo } from '../features/todo/TodoSlice';
 
 const AddTodo = () => {
   const [input, setInput] = useState('');
