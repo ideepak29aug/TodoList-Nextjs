480e5d3c2c38ec7013457947d8f02dae6366d17f
ideepak_29aug <djangir2908@gmail.com>
2024-11-20 13:54:55 +0530

    error solved 7

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
-    webpack: (config, { isServer }) => {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-      
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@': path.join(__dirname, './src'),
-      };
-  
-      return config;
-    },
-  }
-  
-  module.exports = nextConfig
+  webpack: (config, { isServer }) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.join(__dirname, './src'),
+    };
+
+    return config;
+  },
+};
+
+export default nextConfig;
