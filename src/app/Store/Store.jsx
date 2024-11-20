56341786060042865bf95728407cf73e89059e04
ideepak_29aug <djangir2908@gmail.com>
2024-11-20 11:23:10 +0530

    error solve 1

--- src/app/Store/Store.jsx
+++ src/app/Store/Store.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from '../features/todo/TodoSlice'; // Corrected import path and use default import
+import todoReducer from '../features/todo/TodoSlice'; 
 
 export const store = configureStore({
   reducer: {
