4a903b433cccde4037f30792cf78450a8ce485e0
ideepak_29aug <djangir2908@gmail.com>
2024-11-20 16:14:07 +0530

    change img location

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,17 @@ import Image from "next/image";
 import AddTodo from "./Components/AddTodo";
 import Todo from "./Components/Todo";
 import ClientProvider from "./ClientProvider";
+import favicon from '../../public/favicon.png'
 
 export default function Home() {
   return (
     <ClientProvider>
       <div className='w-screen h-screen bg-slate-700 flex justify-center items-start overflow-hidden'>
         <div className="max-w-5xl w-full py-8 flex flex-col justify-start items-center overflow-hidden">
-          <h1 className="text-white text-3xl flex gap-2 justify-center items-center"><Image src='/favicon.png' alt="" width={50} height={40} /> TODO LIST</h1>
+          <div className="flex justify-center items-center gap-4">
+            <Image src={favicon} alt="" width={50} height={40} />
+            <h1 className="text-white text-3xl"> TODO LIST</h1>
+          </div>
           <AddTodo />
           <Todo />
         </div>
